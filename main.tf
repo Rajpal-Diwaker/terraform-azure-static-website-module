7c1b105be76f802f6b9684095d48b0d51cd8810a
Bhargav Bachina <bhargavbachina@Bhargavs-MacBook-Pro.local>
2021-11-19 21:30:38 -0600

    initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_resource_group" "resource_group" {
+  name  = var.rg-name
+  location = var.location
+}
+
+module "static-website" {
+  source = "./modules/static-website"
+
+  location = var.location
+  rg-name = var.rg-name
+  storage-account-name = var.storage-account-name
+  index_document = var.index_document
+  source_content = var.source_content
+  cdnprofile-name = var.cdnprofile-name
+  cdnprofile-sku = var.cdnprofile-sku
+  cdn-endpoint-name = var.cdn-endpoint-name
+
+  depends_on = [azurerm_resource_group.resource_group]
+}
